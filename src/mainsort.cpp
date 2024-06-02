692446101f2c8418de55cf5838e58c4a223019a0
Siddhanta Adhikari <siddhanta2adhikari@gmail.com>
2024-06-02 10:36:38 +0545

    quicksort impemented

--- src/mainsort.cpp
+++ src/mainsort.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <stdlib.h>
 #include <time.h>
+#include"../header/quicksort.h"
 #include "../header/insertion.h"
 int main()
 {
@@ -17,9 +18,17 @@ int main()
     }
     std::cout << std::endl;
     insertion(arr);
-    std::cout << "After sorting: ";
+    std::cout << "After insertion sorting: ";
      for (int i = 0; i < n; i++) {
         std::cout << arr[i] << " ";
     }
-
+srand(time(NULL));
+    for (int i = 0; i < n; i++) {
+        arr.push_back(rand() % 100);
+    }   
+     std::cout << "Before sorting: ";
+    for (int i = 0; i < n; i++) {
+        std::cout << arr[i] << " ";
+    }
+    quicksort(arr,0,n-1);
 }
