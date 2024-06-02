24ceeab83e4c66253da86d17d921a322d1b83bc4
Siddhanta Adhikari <siddhanta2adhikari@gmail.com>
2024-06-02 10:18:35 +0545

    insertion

--- header/quicksort.h
+++ header/quicksort.h
@@ -1,4 +1,6 @@
-partition(a,high,low)
+
+#include <iostream>
+int partition(a,int high,int low)
 {
     int pivot =high/2;
     int i=low;
@@ -8,7 +10,16 @@ partition(a,high,low)
         while(a[i]<a[pivot])
         {
             i++;
+        }   
+        while(a[j]>a[pivot])
+        {
+            j--;
         }
+        if(i<j)
+        {
+            std::swap(a[i],a[j]);
+        }
+        s
 
     }
 }
